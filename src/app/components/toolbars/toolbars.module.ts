e63334da596cddf76892caef02cd4dfeb378c6ff
Rami Mustaklem <ramimustaklem@hotmail.com>
2018-04-14 22:09:46 +0300

    Dynamic Easy Front-End Dashboard:
    - Changes:
     - Separate DEFED toolbar component in a separate folder
     - Update DEFED toolbar component location urls

--- src/app/components/toolbars/toolbars.module.ts
+++ src/app/components/toolbars/toolbars.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatIconModule } from '@angular/material';
-import { DefedToolbarComponent } from "./defed-toolbar.component";
+import { DefedToolbarComponent } from "./defed-toolbar/defed-toolbar.component";
 
 @NgModule({
   imports: [
